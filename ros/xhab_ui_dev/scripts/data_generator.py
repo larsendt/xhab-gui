3cf6d223fa8c557fe8d30998e1c99902316e84bc
Daniel Zukowski <daniel@aktionlab.com>
2014-03-02 19:15:03 -0700

    added generators and loggers for 4 SPOTs, each with ph, ec, temp, and rh

--- ros/xhab_ui_dev/scripts/data_generator.py
+++ ros/xhab_ui_dev/scripts/data_generator.py
@@ -4,15 +4,61 @@ import math
 from std_msgs.msg import Float32
 
 def talker():
-  pub = rospy.Publisher('dev/data', Float32)
+  ph_pub   = [rospy.Publisher('data/spot1/ph', Float32),
+              rospy.Publisher('data/spot2/ph', Float32),
+              rospy.Publisher('data/spot3/ph', Float32),
+              rospy.Publisher('data/spot4/ph', Float32)]
+  
+  ec_pub   = [rospy.Publisher('data/spot1/ec', Float32),
+              rospy.Publisher('data/spot2/ec', Float32),
+              rospy.Publisher('data/spot3/ec', Float32),
+              rospy.Publisher('data/spot4/ec', Float32)]
+  
+  temp_pub = [rospy.Publisher('data/spot1/temp', Float32),
+              rospy.Publisher('data/spot2/temp', Float32),
+              rospy.Publisher('data/spot3/temp', Float32),
+              rospy.Publisher('data/spot4/temp', Float32)]
+  
+  rh_pub   = [rospy.Publisher('data/spot1/rh', Float32),
+              rospy.Publisher('data/spot2/rh', Float32),
+              rospy.Publisher('data/spot3/rh', Float32),
+              rospy.Publisher('data/spot4/rh', Float32)]
+
   rospy.init_node('data_generator')
-  i = 0
+  
+  ph    = [10, 60, 160, 240]
+  ec    = [50, 0, 300, 250]
+  temp  = [200, 340, 30, 100]
+  rh    = [90, 80, 20, 200]
+
+  offset = 0
   while not rospy.is_shutdown():
-    y = math.sin(math.radians(i)) + 1
-    rospy.loginfo(y)
-    pub.publish(Float32(y))
-    rospy.sleep(1.0)
-    i += 1
+    # ph
+    for i in range (0,4):
+      y = math.sin(math.radians(ph[i]+offset)) + 6
+      rospy.loginfo(y)
+      ph_pub[i].publish(Float32(y))
+  
+    # ec
+    for i in range (0,4):
+      y = (math.sin(math.radians(ec[i]+offset)) + 1 ) / 10 + 0.4 
+      rospy.loginfo(y)
+      ec_pub[i].publish(Float32(y))
+
+    # temp
+    for i in range (0,4):
+      y = (math.sin(math.radians(temp[i]+offset)) + 65) * 1.25
+      rospy.loginfo(y)
+      temp_pub[i].publish(Float32(y))
+
+    # rh
+    for i in range (0,4):
+      y = (math.sin(math.radians(rh[i]+offset)) + 60) * 1.5
+      rospy.loginfo(y)
+      rh_pub[i].publish(Float32(y))
+
+    offset += 1
+    rospy.sleep(10.0)
 
 if __name__ == '__main__':
   try:
--- ros/xhab_ui_dev/scripts/data_logger.py
+++ ros/xhab_ui_dev/scripts/data_logger.py
@@ -4,22 +4,85 @@ import MySQLdb
 import time
 from std_msgs.msg import Float32
 
-db = MySQLdb.connect("localhost","root","","xhab_ui_dev" ) 
-cursor = db.cursor()
+def callback_spot1_ph(data):
+  insert_data('spot1', 'ph', data)
 
-def callback(data):
+def callback_spot2_ph(data):
+  insert_data('spot2', 'ph', data)
+
+def callback_spot3_ph(data):
+  insert_data('spot3', 'ph', data)
+
+def callback_spot4_ph(data):
+  insert_data('spot4', 'ph', data)
+
+def callback_spot1_temp(data):
+  insert_data('spot1', 'temp', data)
+
+def callback_spot2_temp(data):
+  insert_data('spot2', 'temp', data)
+
+def callback_spot3_temp(data):
+  insert_data('spot3', 'temp', data)
+
+def callback_spot4_temp(data):
+  insert_data('spot4', 'temp', data)
+
+def callback_spot1_ec(data):
+  insert_data('spot1', 'ec', data)
+
+def callback_spot2_ec(data):
+  insert_data('spot2', 'ec', data)
+
+def callback_spot3_ec(data):
+  insert_data('spot3', 'ec', data)
+
+def callback_spot4_ec(data):
+  insert_data('spot4', 'ec', data)
+
+def callback_spot1_rh(data):
+  insert_data('spot1', 'rh', data)
+
+def callback_spot2_rh(data):
+  insert_data('spot2', 'rh', data)
+
+def callback_spot3_rh(data):
+  insert_data('spot3', 'rh', data)
+
+def callback_spot4_rh(data):
+  insert_data('spot4', 'rh', data)
+
+def insert_data(source, prop, data):
+  db = MySQLdb.connect("localhost","root","","xhab_ui_dev" ) 
+  cursor = db.cursor()
   try:
-    cursor.execute('INSERT INTO data (timestamp, source, type, value) VALUES(%(timestamp)s, %(source)s, %(type)s, %(value)s)', { 'timestamp': time.time(), 'source': 'spot1', 'type': 'ph', 'value': str(data.data) })
+    cursor.execute('INSERT INTO data (timestamp, source, property, value) VALUES(%(timestamp)s, %(source)s, %(property)s, %(value)s)'
+, { 'timestamp': time.time(), 'source': source, 'property': prop, 'value': data.data })
     db.commit()
   except:
     db.rollback()
-  
-  rospy.loginfo(rospy.get_name() + ": Received %s" % data.data)
+
+  db.close()
 
 def listener():
   rospy.init_node('data_logger', anonymous=True)
-  rospy.Subscriber("dev/data", Float32, callback)
+  rospy.Subscriber("data/spot1/ph", Float32, callback_spot1_ph)
+  rospy.Subscriber("data/spot2/ph", Float32, callback_spot2_ph)
+  rospy.Subscriber("data/spot3/ph", Float32, callback_spot3_ph)
+  rospy.Subscriber("data/spot4/ph", Float32, callback_spot4_ph)
+  rospy.Subscriber("data/spot1/ec", Float32, callback_spot1_ec)
+  rospy.Subscriber("data/spot2/ec", Float32, callback_spot2_ec)
+  rospy.Subscriber("data/spot3/ec", Float32, callback_spot3_ec)
+  rospy.Subscriber("data/spot4/ec", Float32, callback_spot4_ec)
+  rospy.Subscriber("data/spot1/temp", Float32, callback_spot1_temp)
+  rospy.Subscriber("data/spot2/temp", Float32, callback_spot2_temp)
+  rospy.Subscriber("data/spot3/temp", Float32, callback_spot3_temp)
+  rospy.Subscriber("data/spot4/temp", Float32, callback_spot4_temp)
+  rospy.Subscriber("data/spot1/rh", Float32, callback_spot1_rh)
+  rospy.Subscriber("data/spot2/rh", Float32, callback_spot2_rh)
+  rospy.Subscriber("data/spot3/rh", Float32, callback_spot3_rh)
+  rospy.Subscriber("data/spot4/rh", Float32, callback_spot4_rh)
   rospy.spin()
-
+ 
 if __name__ == '__main__':
-  listener()
+  listener() 
