c7b5245a33f72a5d4a350cd2083245bc76c6cc10
Daniel Zukowski <daniel@aktionlab.com>
2014-03-02 19:33:19 -0700

    comment out topic setup...using cusbism to pull data from mysql

--- rails/app/assets/javascripts/ros_topics.js
+++ rails/app/assets/javascripts/ros_topics.js
@@ -1,7 +1,15 @@
-ros = new ROSLIB.Ros({
-  url : 'ws://localhost:9090'
-});
+$(document).ready(function() {
 
-window.topics = {
-  data: new ROSLIB.Topic({ ros: ros, name: '/dev/data', messageType: 'std_msgs/Float32' }) 
-}
+//ros = new ROSLIB.Ros({
+//  url : 'ws://localhost:9090'
+//});
+
+//window.topics = {
+//  data: {
+//    spot1: {
+//      ph: new ROSLIB.Topic({ ros: ros, name: '/data/spot1/ph', messageType: 'std_msgs/Float32' })
+//    }
+//  } 
+//}
+
+});
