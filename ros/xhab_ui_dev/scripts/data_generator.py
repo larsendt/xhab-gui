454aca7a2746f88bf61a9f33bb62dec4d79de8c5
Daniel Zukowski <daniel@aktionlab.com>
2014-03-04 15:45:23 -0700

    added mock data for battery and water levels

--- ros/xhab_ui_dev/scripts/data_generator.py
+++ ros/xhab_ui_dev/scripts/data_generator.py
@@ -24,12 +24,24 @@ def talker():
               rospy.Publisher('data/spot3/rh', Float32),
               rospy.Publisher('data/spot4/rh', Float32)]
 
+  pwr_pub  = [rospy.Publisher('data/spot1/battery', Float32),
+              rospy.Publisher('data/spot2/battery', Float32),
+              rospy.Publisher('data/spot3/battery', Float32),
+              rospy.Publisher('data/spot4/battery', Float32)]
+
+  res_pub  = [rospy.Publisher('data/spot1/res_level', Float32),
+              rospy.Publisher('data/spot2/res_level', Float32),
+              rospy.Publisher('data/spot3/res_level', Float32),
+              rospy.Publisher('data/spot4/res_level', Float32)]
+
   rospy.init_node('data_generator')
   
-  ph    = [10, 60, 160, 240]
-  ec    = [50, 0, 300, 250]
-  temp  = [200, 340, 30, 100]
-  rh    = [90, 80, 20, 200]
+  ph      = [10, 60, 160, 240]
+  ec      = [50, 0, 300, 250]
+  temp    = [200, 340, 30, 100]
+  rh      = [90, 80, 20, 200]
+  pwr     = [0, 100, 300, 200]
+  res     = [100, 300, 200, 0]
 
   offset = 0
   while not rospy.is_shutdown():
@@ -39,23 +51,35 @@ def talker():
       rospy.loginfo(y)
       ph_pub[i].publish(Float32(y))
   
-    # ec
+    # electrical conductivity
     for i in range (0,4):
       y = (math.sin(math.radians(ec[i]+offset)) + 1 ) / 10 + 0.4 
       rospy.loginfo(y)
       ec_pub[i].publish(Float32(y))
 
-    # temp
+    # temperature
     for i in range (0,4):
       y = (math.sin(math.radians(temp[i]+offset)) + 65) * 1.25
       rospy.loginfo(y)
       temp_pub[i].publish(Float32(y))
 
-    # rh
+    # relative humidity
     for i in range (0,4):
       y = (math.sin(math.radians(rh[i]+offset)) + 60) * 1.5
       rospy.loginfo(y)
       rh_pub[i].publish(Float32(y))
+
+    # battery power
+    for i in range (0,4):
+      y = (math.sin(math.radians(pwr[i]+offset)) + 1) / 2
+      rospy.loginfo(y)
+      pwr_pub[i].publish(Float32(y))
+
+    # reservoir level
+    for i in range (0,4):
+      y = (math.sin(math.radians(res[i]+offset)) + 1) / 2
+      rospy.loginfo(y)
+      res_pub[i].publish(Float32(y))
 
     offset += 1
     rospy.sleep(10.0)
--- ros/xhab_ui_dev/scripts/data_logger.py
+++ ros/xhab_ui_dev/scripts/data_logger.py
@@ -52,6 +52,30 @@ def callback_spot3_rh(data):
 def callback_spot4_rh(data):
   insert_data('spot4', 'rh', data)
 
+def callback_spot1_pwr(data):
+  insert_data('spot1', 'battery', data)
+
+def callback_spot2_pwr(data):
+  insert_data('spot2', 'battery', data)
+
+def callback_spot3_pwr(data):
+  insert_data('spot3', 'battery', data)
+
+def callback_spot4_pwr(data):
+  insert_data('spot4', 'battery', data)
+
+def callback_spot1_res(data):
+  insert_data('spot1', 'res_level', data)
+
+def callback_spot2_res(data):
+  insert_data('spot2', 'res_level', data)
+
+def callback_spot3_res(data):
+  insert_data('spot3', 'res_level', data)
+
+def callback_spot4_res(data):
+  insert_data('spot4', 'res_level', data)
+
 def insert_data(source, prop, data):
   db = MySQLdb.connect("localhost","root","","xhab_ui_dev" ) 
   cursor = db.cursor()
@@ -82,6 +106,14 @@ def listener():
   rospy.Subscriber("data/spot2/rh", Float32, callback_spot2_rh)
   rospy.Subscriber("data/spot3/rh", Float32, callback_spot3_rh)
   rospy.Subscriber("data/spot4/rh", Float32, callback_spot4_rh)
+  rospy.Subscriber("data/spot1/battery", Float32, callback_spot1_pwr)
+  rospy.Subscriber("data/spot2/battery", Float32, callback_spot2_pwr)
+  rospy.Subscriber("data/spot3/battery", Float32, callback_spot3_pwr)
+  rospy.Subscriber("data/spot4/battery", Float32, callback_spot4_pwr)
+  rospy.Subscriber("data/spot1/res_level", Float32, callback_spot1_res)
+  rospy.Subscriber("data/spot2/res_level", Float32, callback_spot2_res)
+  rospy.Subscriber("data/spot3/res_level", Float32, callback_spot3_res)
+  rospy.Subscriber("data/spot4/res_level", Float32, callback_spot4_res)
   rospy.spin()
  
 if __name__ == '__main__':
